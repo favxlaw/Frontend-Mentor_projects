2de6140513913a620c143bb18adf1965a27b154d
favxlaw <nmesomafavour2004@gmail.com>
2022-11-26 22:07:42 -0800

    fixed-11

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,5 @@
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+#!/usr/bin/python3
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
